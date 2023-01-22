23c29d6184f2c94ead8c6181b53a6a23906eeb46
sajadssa <sajadsaeediazad0007@gmail.com>
2023-01-22 14:42:55 +0330

    new changes

--- draft.js
+++ draft.js
@@ -2,12 +2,17 @@
 console.log('start');
 //login:email,pass=>api call
 
-function loginUser(email, password,callback){
-    setTimeout(() => {
-        console.log('data is ready');
-        //return {user.email: email}
-        callback({email});
-    }, 3000);
+//1-
+function loginUser(email, password) {
+    return new Promise((resolve, reject) => {
+
+        setTimeout(() => {
+            console.log('data is ready');
+            //return {user.email: email}
+            resolve({ email });
+        }, 1000);
+    })
+
 }
 
 //second function for getting courses by email
@@ -15,47 +20,83 @@ function loginUser(email, password,callback){
 
 
 
+//2-
 
-
-function getUserCourses(email,callback){
-   setTimeout(() => {
-    callback (["course 1","course 2","course 3","course 4"]);
-
+function getUserCourses(email) {
+    return new Promise((resolve,reject)=>{
+ setTimeout(() => {
+            resolve(["course 1", "course 2", "course 3", "course 4"]);
     
-   }, 2000);
+    
+        }, 1000);
+    });
 
 }
-const user= loginUser("sajadex@gmail.com",1234,(user)=>{
-console.log(user.email);
-//call getUserCourses function 
-getUserCourses(user.email,(courses)=>{
-    console.log(courses);
-});
+//3-function for retrieve detail courses
 
+function getDetailCourses(id) {
+    return new Promise((resolve,reject)=>{
+
+
+        setTimeout(() => {
+            resolve("title is react");
     
+    
+        }, 1000);
+    });
 
-});
+}
+
+//consume promise for above example
+loginUser("sajad@ex.com",1234)
+.then((user)=>getUserCourses(user.email))
+.then((courses=>getDetailCourses(courses[0])))
+.then((detail)=>console.log(detail));
+
+// const user= loginUser("sajadex@gmail.com",1234,(user)=>{
+// console.log(user.email);
+// //call getUserCourses function 
+// getUserCourses(user.email,(courses)=>{
+//     console.log(courses);
+//     //call getDetailCourse function 
+//     getDetailCourses(courses[0],(title)=>{
+//         console.log(title);
+//     })
+// });
+
+
+
+
+
+
+// });
+
+//REFACTOR CODE WITH PROMISE
+
+
+
+
 
 console.log('end');
 
 //promise in js
-const promise=new Promise((resolve,reject)=>{
-    setTimeout(() => {
-        const error=false;
-        if(!error){
+// const promise = new Promise((resolve, reject) => {
+//     setTimeout(() => {
+//         const error = false;
+//         if (!error) {
 
-            resolve("title course is react.js");
-        }
-        else{
+//             resolve("title course is react.js");
+//         }
+//         else {
 
-            reject(new Error("User is not logged in"));
-        }
-        
-    }, 2000);
-});
+//             reject(new Error("User is not logged in"));
+//         }
+
+//     }, 3000);
+// });
 
 
-//consume promise
-promise
-.then((data)=>console.log(data))
-.catch((error)=>console.log(error.message));
+// //consume promise
+// promise
+//     .then((data) => console.log(data))
+//     .catch((error) => console.log(error.message));
