86b0be83216b09fb57fcfbfeaa4de0e013734ae8
sajadssa <sajadsaeediazad0007@gmail.com>
2024-04-06 02:33:59 +0330

    youtubeJS_SSD Initialized

---
+++ youtubeJS_SSD/index.js
@@ -0,0 +1,38 @@
+//json:javascript object notation
+//this is json file in below
+const jsObject={
+    name:'shirt',
+    'delivery-time':'1 day',
+    rating:{
+        starts:4.5,
+        count:87
+    }
+    ,fun:function function1 () {
+        console.log('function inside object');
+        
+    }
+
+}
+jsObject.fun();
+//convert jsObject to JSON object?
+/*Sure, to convert a JavaScript object to a JSON string, you can use the JSON.stringify() method. Here's how you can do it with the jsObject: */
+
+const jsonString = JSON.stringify(jsObject);
+console.log(jsonString);
+console.log(typeof jsonString);
+
+/*THE RESULT IS 
+'JSON.stringify(jsObject)'
+JSON={"name":"shirt","delivery-time":"1 day","rating":{"starts":4.5,"count":87}} */
+//JSON DOES NOT SUPPORT FUNCTION
+//type of JSON IS 'String'
+//CONVERT JSON TO JAVASCRIPT OBJECT
+//JSON.parse(json)
+const jsObject2=JSON.parse(jsonString);
+console.log(jsObject2);
+console.log(typeof jsObject2);
+
+/*THE RESULT IS 
+'JSON.parse(jsonString)'
+{name:'shirt', 'delivery-time': '1 day', rating: { starts: 4.5, count: 87 }, fun: function function1 () { console.log('function inside object'); } } */
+//next BUILT-in object localStorage:SAVE VALUES MORE PERMANENTLY
